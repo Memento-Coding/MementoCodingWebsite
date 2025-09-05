99b0e82fca46fad3c75e86eaec51a164200d1dd0
Jhosebro <jhosebro2108@gmail.com>
2025-09-05 07:51:05 -0500

    feat: add new header lines

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, LayoutModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
 export class Hero {
+  isSmallScreen = false;
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.isSmallScreen = result.matches;
+    })
+  }
   protected readonly title = signal('Memento Coding - Agile software development');
   protected readonly subtitle = signal('We build high-quality software solutions tailored to your business needs.');
   protected readonly ctaText = signal('Get Started');
